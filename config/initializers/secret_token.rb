573be21a3ee02cfec03cdddbe4fdd3601a05714d
Zachariah Reitano <zreitano@gmail.com>
2013-10-01 16:51:39 -0400

    First push to heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SplashSite::Application.config.secret_key_base = 'df930c4410e61247f1179581ab2a0f5c4b68cb719685bb2e218f4620cab4464535c300a26df0be6f2481fd5fe932a53245a4e615c5f2e9c9dc4e05b403b049d4'
